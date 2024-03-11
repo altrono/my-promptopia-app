5cd5d88bc3186765f546fc58aac840629d9fa5b4
altrono <hbotema16@gmail.com>
2024-03-11 20:38:34 +0200

    bugs

--- utils/database.js
+++ utils/database.js
@@ -12,7 +12,7 @@ export const connectToDB = async () => {
 
     try {
         await mongoose.connect(
-            process.env.MONGODB_URI,
+            process.env.MONGODB_URI.toString() || 'development',
             {
                 dbName: "share_prompt",
                 useNewUrlParser: true,
