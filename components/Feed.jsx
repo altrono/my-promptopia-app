1c3a151a85e9db3f0799223e651f193e779c65a8
altrono <hbotema16@gmail.com>
2024-03-11 16:59:40 +0200

    finished the app

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,38 +3,80 @@
 import { useState, useEffect } from "react";
 
 import PromptCard from "./PromptCard";
+import { set } from "mongoose";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
       {data.map((post) => (
-        <PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+        <PromptCard 
+          key={post._id} 
+          post={post} 
+          handleTagClick={handleTagClick} 
+          />
       ))}
     </div>
   )
 }
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState('');
-
   const [posts, setPosts] = useState([]);
 
 
-  const handleSearchChange = (e) => {
+
+  const [searchText, setSearchText] = useState('');
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setPosts(data);
+  }
+
+
+  useEffect(()=> {
+    fetchPosts();
+  }, []);
+
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchText, "i");
+
+    return posts.filter((item) => 
+    regex.test(item.creator.username) ||
+    regex.test(item.tag) ||
+    regex.test(item.prompt)
+    
+    ) ;
 
   }
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch('/api/prompt');
-      const data = await response.json();
-      setPosts(data);
 
-    }
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
 
-    fetchPosts();
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
 
-  }, [])
+  }
+
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
 
   return (
     <section className="feed">
@@ -50,10 +92,15 @@ const Feed = () => {
           />
       </form>
 
-      <PromptCardList
+      {searchText ? (
+      <PromptCardList 
+          data={searchedResults}
+          handleTagClick={handleTagClick} 
+          />) :
+          (<PromptCardList
        data={posts}
-       handleTagClick={() => {}} 
-      />
+       handleTagClick={handleTagClick} 
+      />)}
     </section>
   )
 }
--- components/Form.jsx
+++ components/Form.jsx
@@ -19,46 +19,52 @@ const Form = ({
       </p>
       <form 
         onSubmit={handleSubmit}
-        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 
+        glassmorphism">
           <label>
-            <span className="font-satoshi font-semibold text-base text-gray-700">
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
             Your AI Prompt
+          </span>
+
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder='Write your post here'
+            required
+            className='form_textarea '
+          />
+        </label>
+
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Field of Prompt{" "}
+            <span className='font-normal'>
+              (#product, #webdevelopment, #idea, etc.)
             </span>
-            <textarea 
-              value={post.prompt}
-              onChange={(e) => setPost({...post, prompt: e.target.value })}
-              placeholder="Write your prompt here..."
-              required
-              className="form_textarea"
-            />
-          </label>
+          </span>
+          <input
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+            type='text'
+            placeholder='#Tag'
+            required
+            className='form_input'
+          />
+        </label>
 
-          <label>
-            <span className="font-satoshi font-semibold text-base text-gray-700">
-            Tag {` `}
-            <span className="font-normal">(#product,  #webdevelopment,  #idea)</span>
-            </span>
-            <input
-              value={post.tag}
-              onChange={(e) => setPost({...post, tag: e.target.value })}
-              placeholder="#tag"
-              required
-              className="form_input"
-            />
-          </label>
+        <div className='flex-end mx-3 mb-5 gap-4'>
+          <Link href='/' className='text-gray-500 text-sm'>
+            Cancel
+          </Link>
 
-          <div className="flex-end mx-3 mb-5 gap-4">
-            <Link className="text-gray-500 text-sm" href="/" >
-              Cancel
-            </Link>
-
-            <button 
-              type="submit"
-              disabled={submitting}
-              className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white">
-              {submitting ? `${type}...` : type}
-            </button>
-          </div>
+          <button
+            type='submit'
+            disabled={submitting}
+            className='px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white'
+          >
+            {submitting ? `${type}ing...` : type}
+          </button>
+        </div>
 
       </form>
     </section>
